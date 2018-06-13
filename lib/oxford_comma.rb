7370c5a38503302f6395f62f4f1d40b17a786bf9
github username <github email address>
2018-06-12 23:11:40 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array.join
+  elsif array.length == 2
+    return array.join(" and ")
+  elsif array.length >= 3
+    last_element = array[-1]
+    array.pop
+    array << "and #{last_element}"
+    return array.join(", ")
+  end
 end
